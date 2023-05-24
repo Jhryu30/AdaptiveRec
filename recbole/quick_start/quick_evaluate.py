ae4a0e66559a365da669401fe23ad8323f37c10f
Jhryu <jhryu30@snu.ac.kr>
2023-05-24 19:57:53 +0900

    update evaluation

--- recbole/quick_start/quick_evaluate.py
+++ recbole/quick_start/quick_evaluate.py
@@ -18,8 +18,7 @@ import wandb
 
 
 
-
-def run_recbole(model=None, dataset=None, config_file_list=None, config_dict=None, saved=True):
+def run_recbole_eval(model=None, dataset=None, config_file_list=None, config_dict=None, saved=True, log_dir=None):
     r""" A fast running api, which includes the complete process of
     training and testing a model on a specified dataset
 
@@ -28,114 +27,56 @@ def run_recbole(model=None, dataset=None, config_file_list=None, config_dict=Non
         dataset (str): dataset name
         config_file_list (list): config files used to modify experiment parameters
         config_dict (dict): parameters dictionary used to modify experiment parameters
-        saved (bool): whether to save the model
+        saved (bool): whether to save thelog_dir' model
     """
     # configurations initialization
     config = Config(model=model, dataset=dataset, config_file_list=config_file_list, config_dict=config_dict)
-        
-    wandb.init(project='AdaptiveRec', 
-            name=config.model+config['contrast']+config['eval_setting'].split(',')[1],
-            config=config) #, mode='disabled')
     # init_seed(config['seed'], config['reproducibility'])
-
     
-    # logger initialization
-    init_logger(config)
-    logger = getLogger()
+    # # logger initialization
+    # init_logger(config)
+    # logger = getLogger()
 
     import os
-    log_dir = os.path.dirname(logger.handlers[0].baseFilename)
+    # log_dir = os.path.dirname(logger.handlers[0].baseFilename)
     config['log_dir'] = log_dir
-
-    wandb.config.update({'log_dir':log_dir})
     
-    logger.info(config)
+    # logger.info(config)
 
     # dataset filtering
     dataset = create_dataset(config)
-    logger.info(dataset)
+    # logger.info(dataset)
 
     # dataset splitting
     train_data, valid_data, test_data = data_preparation(config, dataset)
 
     # model loading and initialization
     model = get_model(config['model'])(config, train_data).to(config['device'])
-    logger.info(model)
+    # logger.info(model)
     
     # trainer loading and initialization
     trainer = get_trainer(config['MODEL_TYPE'], config['model'])(config, model)
 
     # model training
-    best_valid_score, best_valid_result = trainer.fit(
-        train_data, valid_data, saved=saved, show_progress=config['show_progress']
-    )
-
-
-    import numpy as np
-    import seaborn as sns
-    import matplotlib.pyplot as plt
-    from sklearn.decomposition import TruncatedSVD
-
-    embedding_matrix = model.item_embedding.weight[1:].cpu().detach().numpy()
-    svd = TruncatedSVD(n_components=2)
-    svd.fit(embedding_matrix)
-    comp_tr = np.transpose(svd.components_)
-    proj = np.dot(embedding_matrix, comp_tr)
-    
-    cnt = {}
-    for i in dataset['item_id']:
-        if i.item() in cnt:
-            cnt[i.item()] += 1
-        else:
-            cnt[i.item()] = 1
-    
-    freq = np.zeros(embedding_matrix.shape[0])
-    for i in cnt:
-        freq[i-1] = cnt[i]
-    
-    # freq /= freq.max()
-
-    sns.set(style='darkgrid')
-    sns.set_context("notebook", font_scale=1.8, rc={"lines.linewidth": 3, 'lines.markersize': 20})
-    plt.figure(figsize=(6, 4.5))
-    plt.scatter(proj[:, 0], proj[:, 1], s=1, c=freq, cmap='viridis_r')
-    plt.colorbar()
-    plt.xlim(-2, 2)
-    plt.ylim(-2, 2)
-    # plt.axis('square')
-    # plt.show()
-    plt.savefig(log_dir + '/' + config['model'] + '-' + config['dataset'] + '.pdf', format='pdf', transparent=False, bbox_inches='tight')
-    
-    from scipy.linalg import svdvals
-    svs = svdvals(embedding_matrix)
-    svs /= svs.max()
-    np.save(log_dir + '/sv.npy', svs)
-
-    sns.set(style='darkgrid')
-    sns.set_context("notebook", font_scale=1.8, rc={"lines.linewidth": 3, 'lines.markersize': 20})
-    plt.figure(figsize=(6, 4.5))
-    plt.plot(svs)
-    # plt.show()
-    plt.savefig(log_dir + '/svs.pdf', format='pdf', transparent=False, bbox_inches='tight')
+    # best_valid_score, best_valid_result = trainer.fit(
+    #     train_data, valid_data, saved=saved, show_progress=config['show_progress']
+    # )
+    trainer.resume_checkpoint(resume_file=os.path.join(config['log_dir'], 'model.pth'))
 
     # model evaluation
     test_result = trainer.evaluate(test_data, load_best_model=saved, show_progress=config['show_progress'])
 
-    logger.info(set_color('best valid ', 'yellow') + f': {best_valid_result}')
-    logger.info(set_color('test result', 'yellow') + f': {test_result}')
-    
-    wandb.log({'best_valid_score' : best_valid_score,
-               'valid_score_bigger': config['valid_metric_bigger'],
-               'best_valid_result': best_valid_result,
-                'similarity_thres' : trainer.sim_thres,
-                'test_result' : test_result})
+    # logger.info(set_color('best valid ', 'yellow') + f': {best_valid_result}')
+    # logger.info(set_color('test result', 'yellow') + f': {test_result}')
+
+    wandb.log({'test_result_uniform':test_result})
 
     return {
-        'best_valid_score': best_valid_score,
+        # 'best_valid_score': best_valid_score,
         'valid_score_bigger': config['valid_metric_bigger'],
-        'best_valid_result': best_valid_result,
+        # 'best_valid_result': best_valid_result,
         'test_result': test_result
-    }, log_dir
+    }
 
 
 
--- run_seq.py
+++ run_seq.py
@@ -1,6 +1,7 @@
 import argparse
 
 from recbole.quick_start import run_recbole
+from recbole.quick_start.quick_evaluate import run_recbole_eval
 
 
 if __name__ == '__main__':
@@ -12,4 +13,16 @@ if __name__ == '__main__':
     args, _ = parser.parse_known_args()
 
     config_file_list = args.config_files.strip().split(' ') if args.config_files else None
-    run_recbole(model=args.model, dataset=args.dataset, config_file_list=config_file_list)
+    # result, log_dir = run_recbole(model=args.model, dataset=args.dataset, config_file_list=config_file_list)
+    
+    log_dir = 'log/AdaptiveRec/ml-1m/bs256-lmd0.1-sem0.1-us_x-May-24-2023_02-23-04-lr0.001-l20-tau1-dot-DPh0.5-DPa0.5'
+    
+    # random setting
+    args.eval_setting ='TO_LS,uni100'
+    config_file_list = args.config_files.strip().split(' ') if args.config_files else None
+    run_recbole_eval(model=args.model, dataset=args.dataset, config_file_list=config_file_list, log_dir=log_dir)
+    
+    # popular setting
+    args.eval_setting ='TO_LS,pop100'
+    config_file_list = args.config_files.strip().split(' ') if args.config_files else None
+    run_recbole_eval(model=args.model, dataset=args.dataset, config_file_list=config_file_list, log_dir=log_dir)
