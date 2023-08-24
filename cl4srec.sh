b7071cca9059e547d28e0917d639861125608597
Jae Heyoung Jeon <74503396+Jeon789@users.noreply.github.com>
2023-08-24 18:34:44 +0900

    Delete cl4srec.sh

--- cl4srec.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-batch_size=1024
-dataset='amazon_beauty'
-
-CUDA_VISIBLE_DEVICES=3 python run_seq.py --dataset=$dataset --train_batch_size=$batch_size --lmd=0.1 --lmd_sem=0.1 --model='CL4SRec' --contrast='su' --sim='cos' --gpu_id=0
