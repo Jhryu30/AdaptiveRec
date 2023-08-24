8f79fbbdad228402f31c2b41cdef30d1dc904591
Jae Heyoung Jeon <74503396+Jeon789@users.noreply.github.com>
2023-08-24 18:33:27 +0900

    Update and rename main.sh to reproduce.sh

---
+++ reproduce.sh
@@ -0,0 +1,4 @@
+# contrast options = 'su', 'un', 'us_x'
+python run_seq.py --dataset='ml-1m' --model='AdaptiveRec' --contrast='us_x'
+python run_seq.py --dataset='ml-1m' --model='DuoRec' --contrast='us_x'
+python run_seq.py --dataset='ml-1m' --model='CL4SRec' --contrast='us_x'
